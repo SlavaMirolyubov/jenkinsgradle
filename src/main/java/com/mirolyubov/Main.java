37ea307f4c83628695795c619adbedf26dec2752
Slava <grandwhiskey@yandex.ru>
2019-04-24 12:58:27 +0400

    sss

--- src/main/java/com/mirolyubov/Main.java
+++ src/main/java/com/mirolyubov/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Graddle!");
         System.out.println("changes");
+        System.out.println("changes");
     }
 }

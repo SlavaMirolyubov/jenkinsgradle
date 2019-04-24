a85a192785f1a77e4431bfc55cc942af25449309
Slava <grandwhiskey@yandex.ru>
2019-04-24 12:49:34 +0400

    changes

--- src/main/java/com/mirolyubov/Main.java
+++ src/main/java/com/mirolyubov/Main.java
@@ -3,5 +3,6 @@ package com.mirolyubov;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Graddle!");
+        System.out.println("changes");
     }
 }
